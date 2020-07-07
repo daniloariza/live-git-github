7d3b269f3cba29bdf7e4e3161e5a33002f5ba163
Danilo Ariza <daniloariza@MacBook-Air-de-Danilo.local>
2020-07-07 16:52:18 -0300

    teste ssh

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/teste", (req, res) => {
-  return res.json({ hello: "world_2" });
+  return res.json({ hello: "ssh gerado" });
 });
 
 app.listen(3333);
